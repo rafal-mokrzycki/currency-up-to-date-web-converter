8d66f4e3ce1b516bb0c997791a672d4d2bb24e71
rafal-mokrzycki <55636265+rafal-mokrzycki@users.noreply.github.com>
2022-11-28 15:00:05 +0100

    Add skeleton of the app

--- app.py
+++ app.py
@@ -2,54 +2,18 @@
 """
 To run type: flask --app hello run
 """
-import datetime
-import os
-import smtplib
-import time
-from email import encoders
-from email.mime.base import MIMEBase
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-
-import repackage
-
-repackage.up(1)
-import calendar
-import datetime
-
-import pdfkit
-from flask import Flask, flash, make_response, redirect, render_template, request, url_for
-from flask_login import (
-    LoginManager,
-    UserMixin,
-    current_user,
-    login_required,
-    login_user,
-    logout_user,
-)
-from flask_mail import Mail, Message
-from flask_sqlalchemy import Model, SQLAlchemy
-from sqlalchemy import (
-    Boolean,
-    Column,
-    Date,
-    Float,
-    ForeignKey,
-    Integer,
-    MetaData,
-    String,
-    Table,
-    create_engine,
-    func,
-)
-from sqlalchemy.orm import declarative_base, relationship, scoped_session, sessionmaker
-from werkzeug.security import check_password_hash, generate_password_hash
+from flask import Flask, flash, make_response, redirect, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    currencies = ["PLN", "USD", "GPB"]
     if request.method == "POST":
-        return render_template("index.html")
-    return render_template("index.html")
+        return render_template("index.html", currencies=currencies)
+    return render_template("index.html", currencies=currencies)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
